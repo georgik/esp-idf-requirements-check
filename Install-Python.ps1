e4fc42dd85d66da870dc94b9b2eed15c0f8035ad
Juraj Michálek <juraj.michalek@espressif.com>
2020-12-15 10:58:50 +0100

    add patch number to version 3.10

--- Install-Python.ps1
+++ Install-Python.ps1
@@ -1,10 +1,11 @@
 
 param (
     [string]$PythonVersion="3.8.6",
+    [string]$PythonPatch="",
     [string]$PythonDir="python3"
 )
 
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/${PythonVersion}/python-${PythonVersion}-amd64.exe" -Out "python-amd64.exe"
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/${PythonVersion}/python-${PythonVersion}${PythonPatch}-amd64.exe" -Out "python-amd64.exe"
 .\python-amd64.exe /quiet /passive TargetDir=${pwd}\${PythonDir}
 $InstallerProcess = Get-Process python-amd64
 Wait-Process -Id $InstallerProcess.id
