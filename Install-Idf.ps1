069725680183e5ff70f06073f04eef014398c5e1
Juraj Michalek <juraj.michalek@gmail.com>
2021-03-15 17:34:30 +0100

    fix PowerShell read log sequence

--- Install-Idf.ps1
+++ Install-Idf.ps1
@@ -11,18 +11,23 @@ param (
 "* Installer = $Installer"
 "* IdfVersion = $IdfVersion"
 
+$Directory = (Get-Location).Path
+$LogFile = Join-Path -Path $Directory -ChildPath out.txt
 $ProcessName = (Get-Item $Installer).Basename
 "Waiting for process: $ProcessName"
-&$Installer /VERYSILENT /LOG=out.txt /SUPPRESSMSGBOXES /SP- /NOCANCEL /NORESTART /IDFVERSION=${IdfVersion}
+&$Installer /VERYSILENT /LOG=$LogFile /SUPPRESSMSGBOXES /SP- /NOCANCEL /NORESTART /IDFVERSION=${IdfVersion}
 $InstallerProcess = Get-Process $ProcessName
 Sleep 5
 # Logs must be watched in separate job, because Inno Setup does not allow to print stdout.
-$LogWatcher = Start-Job { Get-Countent ./out.txt -Wait }
+$LogWatcher = Start-Job -ArgumentList $LogFile -ScriptBlock {
+    Param($LogFile)
+    Get-Content -Path $LogFile -Wait
+}
 
 # Wait for installer to finish
 while (!$InstallerProcess.HasExited) {
     Sleep 5
-    Receive-Job $LogWatcher
+    Receive-Job $LogWatcher -Keep
 }
 Wait-Process -Id $InstallerProcess.id
 Stop-Job $LogWatcher
