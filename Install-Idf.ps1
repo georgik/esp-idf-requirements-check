502816329425fa96022a7aaa8373506ed9e07cb2
Juraj Michalek <juraj.michalek@gmail.com>
2021-03-15 16:54:08 +0100

    Use PowerShell job to watch for installer to exist

--- Install-Idf.ps1
+++ Install-Idf.ps1
@@ -13,6 +13,15 @@ param (
 
 $ProcessName = (Get-Item $Installer).Basename
 "Waiting for process: $ProcessName"
-&$Installer /VERYSILENT /SUPPRESSMSGBOXES /SP- /NOCANCEL /NORESTART /IDFVERSION=${IdfVersion}
+&$Installer /VERYSILENT /LOG=out.txt /SUPPRESSMSGBOXES /SP- /NOCANCEL /NORESTART /IDFVERSION=${IdfVersion}
 $InstallerProcess = Get-Process $ProcessName
+Sleep 5
+# Logs must be watched in separate job, because Inno Setup does not allow to print stdout.
+$LogWatcher = Start-Job { Get-Countent ./out.txt -Wait }
+
+# Wait for installer to finish
+while (!$InstallerProcess.HasExited) {
+    Sleep 5
+}
 Wait-Process -Id $InstallerProcess.id
+Stop-Job $LogWatcher
